441317b9e091dbdf05d5c32086bc8a1ad2910b08
luacrz <luanacruzsilva2015@gmail.com>
2023-10-13 13:44:13 -0300

    arquivos

---
+++ README.md
@@ -0,0 +1,30 @@
+# Sistema de Recomendação de Filmes
+
+Este projeto é um sistema de recomendação de filmes que utiliza os algoritmos Naive Bayes e K-NN para fornecer recomendações de filmes com base nos dados do conjunto de dados "movies_metadata.csv" do Kaggle.
+
+## Como Usar
+
+Siga as etapas abaixo para configurar e executar o sistema de recomendação:
+
+1. **Instalação das Bibliotecas Necessárias**
+
+   Certifique-se de ter Python 3.x instalado no seu sistema. Em seguida, instale as bibliotecas necessárias executando:
+
+```pip install -r requirements.txt```
+2. 
+.
+.
+.
+3. 
+.
+.
+4. 
+.
+.
+.
+...
+
+
+## Contribuição
+
+Sinta-se à vontade para contribuir para este projeto. Se você encontrar problemas, pode relatar issues ou enviar pull requests com melhorias.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+## Carregamento dos dados com o Panda
 # Caminho do arquivo CSV
 file_path = r'C:\Users\Luana\Documents\UnB\semestre 7\IIA\movies_metadata.csv'
 
@@ -7,5 +8,16 @@ file_path = r'C:\Users\Luana\Documents\UnB\semestre 7\IIA\movies_metadata.csv'
 # O pandas vai ler o arquivo CSV
 df = pd.read_csv(file_path)
 
-# Só é uma verificação das primeiras linhas do DataFrame para garantir que os dados foram carregados corretamente
+# verificação 
 print(df.head())
+
+## Pré-processamento dos Dados
+# para lidar com valores ausentes
+#df = df.fillna(df.mean())
+
+# selecionar elementos relevantes (possibilidade de alteração, vamos discutir)
+#relevant_columns = ['title', 'genres', 'cast', 'director', 'vote_average']
+#df = df[relevant_columns]
+
+# converter em lista de generos
+#df['genres'] = df['genres'].apply(lambda x: x.split('|'))
