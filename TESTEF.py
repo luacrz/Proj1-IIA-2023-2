f029b99f144c30ba55ff487e710aa7fd42825326
Lucas de Oliveira Silva <lucasoliver3500@gmail.com>
2023-10-17 04:55:01 -0300

    Bayes pronto, metricas gerais

--- TESTEF.py
+++ TESTEF.py
@@ -1,16 +1,17 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_curve, auc
-from sklearn.feature_extraction.text import CountVectorizer
 import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_curve, auc
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MultiLabelBinarizer
 
-# Carregar os dados
-movies_metadata = pd.read_csv('movies_metadata.csv')
+# Carregue o conjunto de dados
+df = pd.read_csv('movies_metadata.csv')
 ratings_df = pd.read_csv('ratings_small.csv')
 
 # Engenharia de Recursos: Calcula a média das classificações de usuários por filme
-# e a quantidade de usuarios que avaliaram cada filme
 average_ratings = ratings_df.groupby('movieId')['rating'].agg(['mean', 'count']).reset_index()
 average_ratings.rename(columns={'mean': 'average_rating', 'count': 'num_ratings'}, inplace=True)
 
@@ -18,79 +19,85 @@ average_ratings.rename(columns={'mean': 'average_rating', 'count': 'num_ratings'
 average_ratings = average_ratings[average_ratings['num_ratings'] >= 20]
 
 # Remover entradas que não são convertíveis em inteiros
-movies_metadata = movies_metadata[movies_metadata['id'].str.isnumeric()]
+df = df[df['id'].str.isnumeric()]
 
 # Converter a coluna 'id' para int64
-movies_metadata['id'] = movies_metadata['id'].astype('int')
+df['id'] = df['id'].astype('int64')
 
 # Combinar os dados de filmes com as médias das avaliações
-movies_metadata = movies_metadata.merge(average_ratings, left_on='id', right_on='movieId', how='inner')
+df = df.merge(average_ratings, left_on='id', right_on='movieId', how='inner')
 
+# Definir os gêneros desejados
+genre_keywords = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama']
 
-# Atribuir rótulos aos filmes com base nos gêneros
-target_genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama', 'Family', 'Fantasy', 
-                 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 
-                 'Thriller', 'War', 'Western', 'NewGenre']
-
+# Função para atribuir rótulos com base nos gêneros
 def assign_genre_label(genres):
+    target_genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama']
+    
     genre_list = eval(genres)  # Converte a string de gêneros em uma lista de dicionários
     
-    for genre in genre_list:
-        if genre['name'] in target_genres:
-            return genre['name']
+    assigned_genres = [genre['name'] for genre in genre_list if genre['name'] in target_genres]
     
-    return 'Other'
+    if assigned_genres:
+        return assigned_genres
+    else:
+        return ['Other']
 
-# print(f'Antes\n {movies_metadata.head(10)}')
-movies_metadata['label'] = movies_metadata['genres'].apply(assign_genre_label)
-# print(f'Depois\n {movies_metadata.head(10)}')
+# Atribuir rótulos aos filmes com base em gêneros
+df['label'] = df['genres'].apply(assign_genre_label)
 
-# Criar o vetorizador de gêneros
-vectorizer = CountVectorizer(vocabulary=target_genres)
-X = vectorizer.fit_transform(movies_metadata['label']).toarray()
+# Usar MultiLabelBinarizer para binarizar os rótulos
+mlb = MultiLabelBinarizer()
+labels_binarized = mlb.fit_transform(df['label'])
 
-# Dividir os dados em conjunto de treinamento e teste
-X_train, X_test, y_train, y_test = train_test_split(X, movies_metadata['label'], test_size=0.2, random_state=42)
+# Criar um DataFrame com os rótulos binarizados
+labels_df = pd.DataFrame(labels_binarized, columns=mlb.classes_)
 
-# Treinar o modelo Naive Bayes
-naive_bayes = MultinomialNB()
-naive_bayes.fit(X_train, y_train)
+# Concatenar os DataFrames
+X_combined = pd.concat([df[genre_keywords], df['average_rating'], labels_df], axis=1)
 
-# Avaliar o modelo
-y_pred = naive_bayes.predict(X_test)
+# Dividir os Dados em Conjunto de Treinamento e Teste
+X_train, X_test, y_train, y_test = train_test_split(X_combined, labels_df, df['average_rating'], test_size=0.2, random_state=42)
 
-# Calcular métricas de avaliação
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Acurácia: {accuracy:.2f}')
+# Substitua valores ausentes com a média
+imputer = SimpleImputer(strategy='mean')
+X_train_imputed = imputer.fit_transform(X_train)
+X_test_imputed = imputer.transform(X_test)
 
-for genre in target_genres:
-    y_true = (y_test == genre).astype(int)
-    y_pred_class = (y_pred == genre).astype(int)
+# Criar e treinar o modelo HistGradientBoostingClassifier
+hgbc_classifier = HistGradientBoostingClassifier()
+hgbc_classifier.fit(X_train_imputed, y_train)
 
-    precision = precision_score(y_true, y_pred_class)
-    recall = recall_score(y_true, y_pred_class)
-    f1 = f1_score(y_true, y_pred_class)
+# Acurácia do Modelo
+accuracy = hgbc_classifier.score(X_test_imputed, y_test)
+print(f'Acurácia do Modelo: {accuracy:.2f}')
+
+# Fazer uma previsão
+sample_genre_vector = pd.DataFrame(0, index=[0], columns=genre_keywords)  # Inicializa com zeros
+sample_genre_vector['Drama'] = 1  # Adiciona 1 para o gênero 'Drama'
+sample_genre_vector['average_rating'] = df['average_rating'].mean()  # Adiciona a média das avaliações
+predicted_ratings = hgbc_classifier.predict(sample_genre_vector)
+print('Gêneros Previstos:', mlb.inverse_transform(predicted_ratings))
+
+# Recomendar filmes com base nos gêneros previstos
+predicted_movies = labels_df[mlb.inverse_transform(predicted_ratings)[0]]
+recommended_movies = df.iloc[predicted_movies.index][['title', 'genres']]
+print('Filmes Recomendados:')
+print(recommended_movies.head(10))
+
+# Fazer previsões
+y_pred = hgbc_classifier.predict(X_test_imputed)
+
+# Calcular precisão, revocação e F1 para cada gênero
+for i, genre in enumerate(mlb.classes_):
+    y_true = y_test[:, i]
+    y_pred_class = y_pred[:, i]
+
+    precision = precision_score(y_true, y_pred_class, zero_division=1)
+    recall = recall_score(y_true, y_pred_class, zero_division=1)
+    f1 = f1_score(y_true, y_pred_class, zero_division=1)
 
     print(f'Gênero: {genre}')
     print(f'Precisão: {precision:.2f}')
     print(f'Revocação: {recall:.2f}')
     print(f'Pontuação F1: {f1:.2f}')
-
-# Calcular a curva ROC para o gênero 'Action'
-y_true_action = (y_test == 'Action').astype(int)
-y_prob_action = naive_bayes.predict_proba(X_test)[:, target_genres.index('Action')]
-
-fpr, tpr, thresholds = roc_curve(y_true_action, y_prob_action)
-roc_auc = auc(fpr, tpr)
-
-# Plotar a curva ROC
-plt.figure()
-plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'Curva ROC (área = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('Taxa de Falsos Positivos')
-plt.ylabel('Taxa de Verdadeiros Positivos')
-plt.title('Curva ROC para a classe "Action"')
-plt.legend(loc='lower right')
-plt.show()
--- naive_bayes.py
+++ naive_bayes.py
@@ -2,9 +2,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import HistGradientBoostingClassifier
-from sklearn.metrics import precision_score, recall_score, f1_score, roc_curve, auc
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_curve, auc, accuracy_score, classification_report
 from sklearn.impute import SimpleImputer
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.naive_bayes import MultinomialNB
 
 # Carregue o conjunto de dados
 df = pd.read_csv('movies_metadata.csv')
@@ -34,10 +36,37 @@ df = df.merge(average_ratings, left_on='id', right_on='movieId', how='inner')
 
 
 # Função para atribuir rótulos com base nos gêneros
+def assign_genre_labels(genres):
+    target_genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama']
+    
+    genre_list = eval(genres)  # Converte a string de gêneros em uma lista de dicionários
+    
+    # Cria uma lista com os gêneros presentes no filme
+    labels = [genre['name'] for genre in genre_list if genre['name'] in target_genres]
+    
+    return labels
+
+# Aplicar a função para obter as labels em formato de lista
+df['label'] = df['genres'].apply(assign_genre_labels)
+
+# Converter a lista de listas em uma matriz binária
+mlb = MultiLabelBinarizer()
+X = mlb.fit_transform(df['label'])
+
+# Definir os nomes das colunas
+genre_keywords = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama']
+
+# Criar um DataFrame
+X_df = pd.DataFrame(X, columns=genre_keywords)
+
+# Concatenar com a coluna de avaliação média
+X_combined = pd.concat([X_df, df['average_rating']], axis=1)
+# print(X_combined.head(10))
+# print(df['label'].head(10))
+
+# Função para atribuir rótulos com base nos gêneros
 def assign_genre_label(genres):
-    target_genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama', 'Family', 
-    'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 
-    'Thriller', 'War', 'Western', 'NewGenre']
+    target_genres = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama']
     
     genre_list = eval(genres)  # Converte a string de gêneros em uma lista de dicionários
     
@@ -46,94 +75,61 @@ def assign_genre_label(genres):
             return genre['name']
     
     return 'Other'
-
+    
 # Atribuir rótulos aos filmes com base em gêneros
 df['label'] = df['genres'].apply(assign_genre_label)
-
-# Criar uma lista de todos os gêneros
-genre_keywords = ['Action', 'Adventure', 'Animation', 'Comedy', 'Drama', 'Family', 
-'Fantasy', 'History', 'Horror', 'Music', 'Mystery', 'Romance', 'Science Fiction', 
-'Thriller', 'War', 'Western', 'NewGenre']
-
-# Criar um vetorizador de gêneros
-vectorizer = CountVectorizer(vocabulary=genre_keywords)
-
-# Transformar os gêneros e as médias das classificações em recursos
-X = vectorizer.transform(df['label'])  # Recursos de gênero
-
-# Converter a matriz esparsa em um DataFrame
-X_df = pd.DataFrame(X.toarray())
-
-#print(X_df.head(10))
-
-# Definir os nomes das colunas no DataFrame
-X_df.columns = genre_keywords
-
-# Concatenar os DataFrames
-X_combined = pd.concat([X_df, df['average_rating']], axis=1)
-print(X_combined.head(10))
-
+print(df.head(10))
+    
 # Dividir os Dados em Conjunto de Treinamento e Teste
 X_train, X_test, y_train, y_test = train_test_split(X_combined, df['label'], test_size=0.2, random_state=42)
 
 # print(train_test_split(X_combined, df['label'], test_size=0.2, random_state=42))
-
-# Substitua valores ausentes com a média
-imputer = SimpleImputer(strategy='mean')
-X_train_imputed = imputer.fit_transform(X_train)
-X_test_imputed = imputer.transform(X_test)
-
-# Criar e treinar o modelo HistGradientBoostingClassifier
-hgbc_classifier = HistGradientBoostingClassifier()
-hgbc_classifier.fit(X_train_imputed, y_train)
-
+print(X_train)
+print(y_train)
+# Treinar o modelo Naive Bayes
+naive_bayes_model = MultinomialNB()
+naive_bayes_model.fit(X_train, y_train)
 
 # Acurácia do Modelo
-accuracy = hgbc_classifier.score(X_test_imputed, y_test)
+accuracy = naive_bayes_model.score(X_test, y_test)
 print(f'Acurácia do Modelo: {accuracy:.2f}')
 
+# Criar um vetorizador de gêneros
+vectorizer = CountVectorizer(vocabulary=genre_keywords)
+
 # Fazer uma previsão
-sample_genre_vector = vectorizer.transform(['Action Adventure Western NewGenre'])
-sample_genre_vector = pd.concat([pd.DataFrame(sample_genre_vector.toarray()), pd.DataFrame([df['average_rating'].mean()])], axis=1)
-predicted_genre = hgbc_classifier.predict(sample_genre_vector)
+sample_genre_vector = mlb.transform([['Action', 'Adventure', 'Western']])  # Gêneros do exemplo
+sample_genre_vector_with_rating = np.append(sample_genre_vector, [[3.5]], axis=1)  # Adicionar a avaliação média
+
+predicted_genre = naive_bayes_model.predict(sample_genre_vector_with_rating)
 print(f'Gênero Previsto: {predicted_genre[0]}')
 
 # Recomendar filmes com base no gênero previsto
-recommended_movies = df[df['label'] == predicted_genre[0]]
+recommended_movies = df[df['label'] == predicted_genre[0]].head(10)
 print('Filmes Recomendados:')
-print(recommended_movies[['title', 'genres']].head(10))
+print(recommended_movies[['title', 'genres']])
 
-# Fazer previsões
-y_pred = hgbc_classifier.predict(X_test)
+# Fazer previsões no conjunto de teste
+y_pred = naive_bayes_model.predict(X_test)
 
 # Calcular precisão, revocação e F1 para cada classe
-for genre in genre_keywords:
-    y_true = (y_test == genre).astype(int)
-    y_pred_class = (y_pred == genre).astype(int)
-    '''
-    print(f'Y TRUE\n{y_true}')
-    print(f'Y test\n{y_test}')
-    print(f'Y genre\n{genre}')
-    print(f'Y COMPARAÇÃO\n{(y_test == genre).astype(int)}')
-    #print(f'Y pred\n{y_pred}')
-    #print(f'Y_pred_class\n{y_pred_class}')
-    '''
-    precision = precision_score(y_true, y_pred_class)
-    recall = recall_score(y_true, y_pred_class)
-    f1 = f1_score(y_true, y_pred_class)
+precision = precision_score(y_test, y_pred, average='weighted')
+recall = recall_score(y_test, y_pred, average='weighted')
+f1 = f1_score(y_test, y_pred, average='weighted')
 
-    print(f'Gênero: {genre}')
-    print(f'Precisão: {precision:.2f}')
-    print(f'Revocação: {recall:.2f}')
-    print(f'Pontuação F1: {f1:.2f}')
+print(f'Precisão: {precision:.2f}')
+print(f'Revocação: {recall:.2f}')
+print(f'Pontuação F1: {f1:.2f}')
 
-# Curva ROC (apenas para uma classe)
-y_true = (y_test == 'Drama').astype(int)
-y_prob = hgbc_classifier.predict_proba(X_test)[:, genre_keywords.index('Drama')]
+# Curva ROC
+# Vamos considerar a classe 'Drama' para a curva ROC (substitua por outra classe se desejar)
+y_true_drama = (y_test == 'Drama').astype(int)
+y_scores_drama = naive_bayes_model.predict_proba(X_test)[:, genre_keywords.index('Drama')]
 
-fpr, tpr, thresholds = roc_curve(y_true, y_prob)
+fpr, tpr, thresholds = roc_curve(y_true_drama, y_scores_drama)
 roc_auc = auc(fpr, tpr)
 
+# Plotar a curva ROC
 plt.figure()
 plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'Curva ROC (área = {roc_auc:.2f})')
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
